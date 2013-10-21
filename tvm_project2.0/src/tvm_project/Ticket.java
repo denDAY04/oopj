9a499203d90da39f310c51fb6760f961e39627f6
stensig04@gmail.com <stensig04@gmail.com@2b5b00e0-a592-e1ba-476f-276ff1d89472>
2013-10-21 12:18:59 +0000

    Ticket-Vending-Machine version 2.0 for 2nd semester project.

---
+++ tvm_project2.0/src/tvm_project/Ticket.java
@@ -0,0 +1,111 @@
+package tvm_project;
+import java.sql.Timestamp;
+import java.util.*;
+
+public class Ticket 
+{
+    private int pricePerZone;
+    private String typeDA;
+    private String typeENG;
+    private int startZone;
+    private int zones;
+    private int amountTickets;
+    private String timeStamp;
+    
+    /**
+     * Standard constructor.
+     * @param pricePerZone
+     * @param typeDA
+     * @param typeENG
+     * @param startZone
+     */
+    public Ticket(int pricePerZone, String typeDA, String typeENG,              //creates a new ticket for the createdTickets 
+                                         int startZone)
+    {
+        this.pricePerZone = pricePerZone;
+        this.typeDA = typeDA;
+        this.typeENG = typeENG;
+        this.startZone = startZone;
+    }
+    /**
+     * Constructor for ShoppingBasket.
+     * @param t
+     * @param amountTickets
+     * @param zones
+     */
+    public Ticket(Ticket t, int amountTickets, int zones)                       // creates a new ticket for the shopping cart, using a ticket from the createdTickets.
+    {
+        this.startZone = t.getStartZone();
+        this.zones = t.getZones();
+        this.pricePerZone = t.getPricePerZone();
+        this.typeDA = t.getTypeDA();
+        this.typeENG = t.getTypeENG();
+        this.amountTickets = amountTickets;
+        this.zones = zones;
+    }
+
+    /**
+    * Timestamping method..
+    */
+    public void setTimeStamp()                                                  // asks a tiket to timestamp itself.
+    {
+        java.util.Date date = new java.util.Date();
+        timeStamp = new Timestamp(date.getTime()).toString().substring(0,16);
+    }
+    /**
+     * Timestamping accessor method.
+     * @return
+     */
+    public String getTimeStamp()
+    {
+        return timeStamp;
+    }
+    /**
+     * Type accessor method.
+     * @return
+     */
+    public String getTypeDA()
+    {
+        return typeDA;
+    }
+    /**
+     * Type English accessor method.
+     * @return
+     */
+    public String getTypeENG()
+    {
+        return typeENG;
+    }
+    /**
+     * Zones accessor method.
+     * @return
+     */
+    public int getZones()
+    {
+        return zones;
+    }
+    /**
+     * Start zone accessor method.
+     * @return
+     */
+    public int getStartZone()
+    {
+        return startZone;
+    }
+    /**
+     * price per zone accessor method.
+     * @return
+     */
+    public int getPricePerZone()
+    {
+        return pricePerZone;
+    }
+    /**
+     * amount of tickets accessor method.
+     * @return
+     */
+    public int getAmountTickets()
+    {
+        return amountTickets;
+    }
+}
