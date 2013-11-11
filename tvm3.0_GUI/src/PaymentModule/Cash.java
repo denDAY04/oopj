f074eeb8e903aa49f754a9b4f6895cb2476ead54
stensig04@gmail.com <stensig04@gmail.com@2b5b00e0-a592-e1ba-476f-276ff1d89472>
2013-11-11 14:54:25 +0000

    Payment simulations are WIP; their classes needs rework to fit GUI.

--- tvm3.0_GUI/src/PaymentModule/Cash.java
+++ tvm3.0_GUI/src/PaymentModule/Cash.java
@@ -3,80 +3,34 @@ import Interface.UserInterface;
 
 public class Cash extends Payment
 {
-    public Cash(UserInterface UI)
+    int moneyNeeded = 0;
+    
+    /**
+     * Constructor. Stores the total price in a field variable.
+     * @param totalPrice The total price that needs to be paid.
+     */
+    public Cash(int totalPrice)
     {
-        super(UI);
+        this.moneyNeeded = totalPrice;
     }
     
-    @Override
-    public boolean MakePayment(int totalPrice, int language)
-    {
-        int canclePayment = 9999;       // input required to cancel payment
-        if (language == 1)
-        {
-            UI.printLn("At betale: "+totalPrice+" DKK.");
-            UI.printLn("Indkast penge!");
-            UI.printLn("Tast 9999 for at annullere købet.");
-            while (totalPrice>0)
-            {
-                paid = UI.getInt(); // check for invalid value
-                
-                while (paid <= 0) 
-                {
-                    UI.printLn("Fejl: forkert indtastning. ");
-                    UI.printLn("Prøv igen.");
-                    paid = UI.getInt();
-                } 
-                
-                if (paid==canclePayment) 
-                {   //cancel purchase
-                    UI.printLn("Køb afbrudt.");
-                    UI.printLn("Modtag venligst dine penge retur.");
-                    return false;
-                }
-                   
-                totalPrice -= paid;
-                int totalPrice2 = totalPrice;
-                if (totalPrice2 <0) totalPrice2 = 0; 
-                UI.printLn("Indkastet: "+paid+" DKK, Mangler: "+totalPrice2+" DKK.");
-            }
-            if (totalPrice<0)
-             UI.printLn("Modtag retur: " + (totalPrice*-1)+" Kr.");
-
-            UI.printLn("Betaling gennemført!");
-        }
-        else if (language == 2)
-        {
-            UI.printLn("Required payment: "+totalPrice+" DKK.");
-            UI.printLn("Insert cash!");
-            UI.printLn("Type 9999 to cancel the purchase.");
-            while (totalPrice>0)
-            {
-                paid = UI.getInt();
-                 while (paid <= 0) 
-                 {
-                   UI.printLn("Error invalid input.");
-                   UI.printLn("Try again.");
-                   paid = UI.getInt();
-                 }
-                 
-                 if (paid==canclePayment) 
-                 {
-                   UI.printLn("Purchase canceled.");
-                   UI.printLn("Please accept your money back.");
-                   return false;
-                 }
-
-                 totalPrice -= paid;
-                 int totalPrice2 = totalPrice;
-                 if (totalPrice2 <0) totalPrice2 = 0; 
-                 UI.printLn("Inserted: "+paid+" DKK, Remaining: "+totalPrice2+" DKK.");
-            }
-             if (totalPrice<0)
-             UI.printLn("Accept return: " + (totalPrice*-1)+" DKK");
-
-            UI.printLn("Payment completed!");
-        }
-        return true;
+    /**
+     * Is empty and should not be used.
+     * @return False at all time due to empty body.
+     */
+    public boolean MakePaymentBool() {
+        return false;
+    }
+    
+    /**
+     * Subtracts the parameter int from the price needed to be paid.
+     * @param insertedmoney The number in integer to be subtracted.
+     * @return The new value of the money needed to be paid.
+     */
+    public int MakePaymentInt(int insertedmoney) {
+        moneyNeeded -= insertedmoney;
+        return moneyNeeded;
     }
 }
+    
+   
--- tvm3.0_GUI/src/PaymentModule/Payment.java
+++ tvm3.0_GUI/src/PaymentModule/Payment.java
@@ -1,19 +1,11 @@
 package PaymentModule;
 import Interface.UserInterface;
-public class Payment 
+public abstract class Payment 
 {
-    UserInterface UI;
-    public Payment(UserInterface UI)
-    {
-        this.UI = UI;
-    }
+    public Payment() {}
     
-    protected int paid = 0;
+    public boolean MakePaymentBool;
+    public int MakePaymentInt;
     
-    public boolean MakePayment(int totalPrice, int language)
-    {
-        System.err.println("Error 21 - Running master class, must run sub class.");
-        return false;
-    }
     
 }
