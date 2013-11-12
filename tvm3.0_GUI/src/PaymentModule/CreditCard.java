a09d7ec57e2fd04d22fcb3b311bc6c696486a553
stensig04@gmail.com <stensig04@gmail.com@2b5b00e0-a592-e1ba-476f-276ff1d89472>
2013-11-12 13:46:15 +0000

    Finished payment simulations (all three). Back button in setup2 still bugged.

--- tvm3.0_GUI/src/PaymentModule/CreditCard.java
+++ tvm3.0_GUI/src/PaymentModule/CreditCard.java
@@ -3,16 +3,34 @@ package PaymentModule;
 public class CreditCard extends Payment
 {
     private int pinCode;
-    private String cardPinCode;
+    private String wrongPin;
     
-    public CreditCard()
+    /**
+     * Make a credit card simulation instance. The parameter simulates a
+     * pin that will trigger a wrong-pin simulation.
+     * @param wrontPin Integer to trigger a failed payment instance.
+     */
+    public CreditCard(String wrongPin)
     {
-        
+        this.wrongPin = wrongPin;
     }
     
     
-    public boolean MakePaymentBool()
+    public boolean MakePaymentBool(String pin)
     {
-    return false;    
+        if (pin.length() != 4) {     //If pin is too short or too long
+            return false;
+        } 
+        
+        if (pin.equals(wrongPin)) {  //Simulate a wrong pin-instance
+            return false;
+        } else {
+            try {
+                Integer.parseInt(wrongPin);
+            } catch (NumberFormatException e) {     // pin contains non-int
+                return false;
+            }
+            return true;    //Simulate correct pin
+        }
     }
 }
--- tvm3.0_GUI/src/PaymentModule/paySMS.java
+++ tvm3.0_GUI/src/PaymentModule/paySMS.java
@@ -2,20 +2,47 @@ package PaymentModule;
 
 public class paySMS extends Payment
 {
-    private int cellNumber;
-    private String phoneNumber;
-    private String confirmationSMS;
-    private String confirmSMS;
-    private String cancelSMS;
-    
-    public paySMS()
+    String failedNUM;
+    String NUMWMinusSpaces = "";
+
+    /**
+     * Constructor with a string argument to create a phone number that will 
+     * trigger an error-simulation.
+     * @param failedNUM String that resembles the phone number which will 
+     * trigger the error-simulation.
+     */
+    public paySMS(String failedNUM)
     {
-        
+        this.failedNUM = failedNUM;
     }
     
     
-    public boolean MakePaymentBool()
+    public boolean MakePaymentBool(String NUM)
     {
-        return false;
+        //Trim spaces
+        for (int i = 0; i < NUM.length(); i++) {
+            if (NUM.charAt(i) != ' ') {
+                NUMWMinusSpaces += NUM.charAt(i);
+            }
+        }
+        
+        // Check if string is 8 characters long
+        if (NUMWMinusSpaces.length() != 8) {
+            return false;
+        }
+        
+        // Check if string cntains letters
+        try {
+           Integer.parseInt(NUMWMinusSpaces);
+        } catch (NumberFormatException e) { 
+           return false;
+        }
+        
+        // Compare to number for forced error-simulation
+        if (NUMWMinusSpaces.equals(failedNUM)) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
