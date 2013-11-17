3449c1a60345eb5cb7aa5776e154e04e29b89087
mazlink@gmail.com <mazlink@gmail.com@2b5b00e0-a592-e1ba-476f-276ff1d89472>
2013-11-17 11:35:06 +0000

    Unit test af Shopping basket done

--- TVM3.0_GUI_b/test/JUnit/JUnit_test_Shopping_basket.java
+++ TVM3.0_GUI_b/test/JUnit/JUnit_test_Shopping_basket.java
@@ -17,70 +17,69 @@ import MachineLogic.Statistics;
 public class JUnit_test_Shopping_basket
 {
     
-       /**
-     * Test intToFourChar
-     *
-     */
+    /**
+    * Test intToFourChar
+    *
+    */
     @Test
     public void testIntToFourChar()
     {
-             CreatedTickets cTickets = new CreatedTickets();
-             Statistics Stat = new Statistics(); 
-             ShoppingBasket sBasket = new ShoppingBasket(cTickets,Stat);
-             
+        CreatedTickets cTickets = new CreatedTickets();
+        Statistics Stat = new Statistics(); 
+        ShoppingBasket sBasket = new ShoppingBasket(cTickets,Stat);
+        
+        //Testing of convertions
         assertEquals("   2", sBasket.intToFourChar(2));
         assertEquals("1234", sBasket.intToFourChar(12345));
         assertEquals("   0", sBasket.intToFourChar(0));
         assertEquals("  -1", sBasket.intToFourChar(-1));
         
-   
     }
     
-           /**
-     * Test convertTo8Lenght
-     *
-     */
+    /**
+    * Test convertTo8Lenght
+    *
+    */
     @Test
     public void testConvertTo8Lenght()
     {
-             CreatedTickets cTickets = new CreatedTickets();
-             Statistics Stat = new Statistics(); 
-             ShoppingBasket sBasket = new ShoppingBasket(cTickets,Stat);
-             
+        CreatedTickets cTickets = new CreatedTickets();
+        Statistics Stat = new Statistics(); 
+        ShoppingBasket sBasket = new ShoppingBasket(cTickets,Stat);
+        
+        //Testing of convertions
         assertEquals("       2", sBasket.convertTo8Lenght("2"));
         assertEquals("   12345", sBasket.convertTo8Lenght("12345"));
         assertEquals("12345678", sBasket.convertTo8Lenght("1234567890"));
         assertEquals("       0", sBasket.convertTo8Lenght("0"));
         assertEquals("      -1", sBasket.convertTo8Lenght("-1"));
-        
    
     }
     
-    
-               /**
-     * Test convertTo8Lenght0
-     *
-     */
+    /**
+    * Test convertTo8Lenght0
+    *
+    */
     @Test
     public void testConvertTo8Lenght0()
     {
-             CreatedTickets cTickets = new CreatedTickets();
-             Statistics Stat = new Statistics(); 
-             ShoppingBasket sBasket = new ShoppingBasket(cTickets,Stat);
-             
+        CreatedTickets cTickets = new CreatedTickets();
+        Statistics Stat = new Statistics(); 
+        ShoppingBasket sBasket = new ShoppingBasket(cTickets,Stat);
+        
+        //Testing of convertions
         assertEquals("00000002", sBasket.convertTo8Lenght0(2));
         assertEquals("00012345", sBasket.convertTo8Lenght0(12345));
         assertEquals("12345678", sBasket.convertTo8Lenght0(1234567890));
         assertEquals("00000000", sBasket.convertTo8Lenght0(0));
         assertEquals("-0000001", sBasket.convertTo8Lenght0(-1));
         
-   
     }
 
-     /**
-     * Test addToCart() & getTotalPrice() & getCart() & clearCart()
-     *
-     */
+    /**
+    * Test addToCart() & getTotalPrice() & getCart() & clearCart()
+    *
+    */
     @Test
     public void testShoppingcartMethods()
     {
@@ -92,15 +91,17 @@ public class JUnit_test_Shopping_basket
              //Creation of ticket
              cTickets.addTicket(20, "test", "testENG", 2);
              
-             //Testing addToCart method
-             sBasket.addToCart(1,1,1);
-             
-             
-             assertEquals(34,sBasket.getTotalPrice());
-             assertEquals(4,sBasket.getCart().get(0).getAmountTickets());
+             //Testing of
+             //addToCart(int ticketIndex,int amountZones,int ticketAmount)
+             //method
+             sBasket.addToCart(0,3,1);
+                   
+             //Test getTotalPrice and getCart method
+             assertEquals(60,sBasket.getTotalPrice());
+             assertEquals(1,sBasket.getCart().get(0).getAmountTickets());
              assertEquals(3,sBasket.getCart().get(0).getZones());
-             assertEquals(30,sBasket.getCart().get(0).getStartZone());
-             assertEquals(2,sBasket.getCart().get(0).getPricePerZone());
+             assertEquals(2,sBasket.getCart().get(0).getStartZone());
+             assertEquals(20,sBasket.getCart().get(0).getPricePerZone());
              assertEquals("test",sBasket.getCart().get(0).getTypeDA());
              assertEquals("testENG",sBasket.getCart().get(0).getTypeENG());
              java.util.Date date = new java.util.Date();
@@ -108,7 +109,13 @@ public class JUnit_test_Shopping_basket
              sBasket.getCart().get(0).setTimeStamp();
              assertEquals(timeStamp,sBasket.getCart().get(0).getTimeStamp());
              
+             //Testing removeTicket method
+             sBasket.removeTicket(0);
+             assertEquals(0,sBasket.getCart().size());
+             
              //Test clearCart method
+             sBasket.addToCart(0,1,1);
+             sBasket.addToCart(0,2,3);
              sBasket.clearCart();
              assertEquals(0,sBasket.getCart().size());
     }
