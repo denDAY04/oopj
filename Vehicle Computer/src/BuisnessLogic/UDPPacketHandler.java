23ac51aa63e125a3934bae43a4a56524514e3724
stensig04@gmail.com <stensig04@gmail.com@2b5b00e0-a592-e1ba-476f-276ff1d89472>
2014-11-10 15:32:21 +0000

    Cleanup in UDP-handler and -manager

--- Vehicle Computer/src/BuisnessLogic/UDPPacketHandler.java
+++ Vehicle Computer/src/BuisnessLogic/UDPPacketHandler.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package BuisnessLogic;
 
 import java.io.ByteArrayInputStream;
@@ -21,54 +16,82 @@ import java.util.logging.Logger;
 
 
 /**
- *
- * @author Stensig
+ * Thread extension class for handling communication with a
+ * <code>VehicleComputer</code> in regards to producing a
+ * <code>TicketList</code> out of a <code>PassengerList</code>.
+ * <p>
+ * This class is meant to be instanciated with each new session with a
+ * vehicle computer client, and terminate itself after having completed its
+ * service of the client, as dictated by a custom protocol.
+ * <p>
+ * @author Andreas Stensig Jensen, on Nov 7, 2014
+ * Contributors:
  */
 public class UDPPacketHandler extends Thread {
 
-//    private boolean newData = true;
     private boolean alive = true;
-//    private final String name;
-    private int destPort;
-    private InetAddress destAddr;
+    private final int destPort;
+    private final InetAddress destAddr;
     private byte[] bufferIn = new byte[UDPTrafficManager.BUFFER_IN_SIZE];
     private byte[] bufferOut = new byte[UDPTrafficManager.BUFFER_OUT_SIZE];
-    private DatagramSocket socket;
+    private final DatagramSocket socket;
     private DatagramPacket packet;
     private JourneyManager journeyManager;
 
 
-    public UDPPacketHandler(DatagramPacket packet, int socketPort, String rmiHost, int rmiPort,
-                            String rmiJourneyManName) throws RemoteException, NotBoundException, SocketException {
-        // Name the thread with the sender-host name for identification
-        //name = packet.getAddress().getHostName();
+    /**
+     * Implementational constructer that incorporates RMI variables.
+     * <p>
+     * @param packet            initial <code>DatagramPacket</code> from client
+     *                          wanted to
+     *                          be serviced.
+     * @param socketPort        the port number on which the handler shall open
+     *                          a
+     *                          <code>DatagramSocket</code>.
+     * @param rmiHost           host name of the service running the RMI
+     *                          registry.
+     * @param rmiPort           port number of the service running the RMI
+     *                          registry.
+     * @param rmiJourneyManName name of the <code>JourneyManager</code>
+     *                          implementation class in the RMI registry.
+     * <p>
+     * @throws RemoteException   if unable to locate RMI registry.
+     * @throws NotBoundException if unable to locate implementation class in the
+     *                           RMI registry.
+     * @throws SocketException   if unable to open a <code>DatagramSocket</code>
+     *                           on the specified port.
+     */
+    public UDPPacketHandler(DatagramPacket packet, int socketPort,
+                            String rmiHost, int rmiPort,
+                            String rmiJourneyManName) throws RemoteException,
+                                                             NotBoundException,
+                                                             SocketException {
         this.packet = packet;
         destPort = packet.getPort();
         destAddr = packet.getAddress();
         journeyManager = new JourneyManager(rmiHost, rmiPort, rmiJourneyManName);
         socket = new DatagramSocket(socketPort);
     }
-    
+
     /**
-     * Test constructor for testing. Ignores RMI initialization. Use for 
-     * internal testing that doesn't concern itself with RMI. 
-     * @param packet initial <code>DatagramPacket</code> from client wanted to
-     * be serviced.
-     * @param socketPort the port number on which the handler shall open a 
-     * <code>DatagramSocket</code>. 
+     * Test constructor for testing. Ignores RMI initialization. Use for
+     * internal testing that doesn't concern itself with RMI.
+     * <p>
+     * @param packet     initial <code>DatagramPacket</code> from client wanted
+     *                   to
+     *                   be serviced.
+     * @param socketPort the port number on which the handler shall open a
+     *                   <code>DatagramSocket</code>.
+     * <p>
      * @throws SocketException if the socket is in use.
      */
-    public UDPPacketHandler(DatagramPacket packet, int socketPort) throws SocketException {
+    public UDPPacketHandler(DatagramPacket packet, int socketPort) throws
+            SocketException {
         this.packet = packet;
         destPort = packet.getPort();
         destAddr = packet.getAddress();
         socket = new DatagramSocket(socketPort);
     }
-
-// Legacy method
-//    public String name() {
-//        return name;
-//    }
 
     /**
      * Main flow of the the thread. Will continue to run, processing data
@@ -82,19 +105,16 @@ public class UDPPacketHandler extends Thread {
             processDatagram();
         } catch (IOException ex) {
             /*
-            If first datagram is dropped then client won't know the port of 
-            the handler. Thus the handler serves no purpose as the client will 
-            contant UDPTrafficManager again. There, kill the handler. 
-            */
+             If first datagram is dropped then client won't know the port of 
+             the handler. Thus the handler serves no purpose as the client will 
+             contant UDPTrafficManager again. There, kill the handler. 
+             */
             System.err.println("-- UDPPacketHandler --");
             System.err.println("I/O exception in initial datagram.");
             System.err.println("Killing thread.");
             killThread();
         }
         while (alive) {
-//            if (newData) {
-//                processDatagram();
-//            }
             try {
                 socket.receive(packet);
                 processDatagram();
@@ -113,20 +133,6 @@ public class UDPPacketHandler extends Thread {
      */
     private void killThread() {
         alive = false;
-    }
-
-// might not be needed - legacy method from when all com was through 
-// UDPTrafficManager
-    /**
-     * Put new data in the form of a <code>DatagramPacket</code> in the handler
-     * thread. The new data will be processed as soon as the thread is ready
-     * and done with any current data processing.
-     * <p>
-     * @param packet new <code>DatagramPacket</code> to process.
-     */
-    public void recieveDatagram(DatagramPacket packet) {
-//        this.packet = packet;
-//        newData = true;
     }
 
     /**
@@ -153,30 +159,20 @@ public class UDPPacketHandler extends Thread {
                     log(Level.SEVERE, null, ex);
         }
         System.err.println(s);
-        
+
         // Test reply back to client
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         ObjectOutputStream oos = new ObjectOutputStream(bos);
         String replyMsg = "This is the Handler replying";
         oos.writeObject(replyMsg);
         bufferOut = bos.toByteArray();
-        
-        DatagramPacket replyPacket = new DatagramPacket(bufferOut, bufferOut.length,
+
+        DatagramPacket replyPacket = new DatagramPacket(bufferOut,
+                                                        bufferOut.length,
                                                         destAddr, destPort);
         socket.send(replyPacket);
-        
+
         killThread();
-//        newData = false;
-    }
-
-
-    // send Datagram to VehicleComputer
-    public void recieveTicketList(TicketList tickets) {
-
-    }
-
-    public void sendPassengerlist(PassengerList passengers) {
-
     }
 
 }
--- Vehicle Computer/src/BuisnessLogic/UDPTrafficManager.java
+++ Vehicle Computer/src/BuisnessLogic/UDPTrafficManager.java
@@ -1,8 +1,6 @@
 package BuisnessLogic;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -11,7 +9,17 @@ import java.net.UnknownHostException;
 
 
 /**
- * {Insert class description here}
+ * Manager for incoming serivce requests from <code>VehicleComputer</code>
+ * clients. This class captures the UDP request and instanciates a new instance
+ * of of the <code>UDPPacketHandler</code> class. This class is then passed
+ * the request for service and will service the client in a seperate thread.
+ * <p>
+ * NOTE that this class does not check the incoming UDP datagrams for propper
+ * request syntax as dictated by a custom protocol. Instead, this is left for
+ * the designated handler of the request.
+ * <p>
+ * Furthermore, this class cotains the main method for starting this part of the
+ * business logic back-end.
  * <p>
  * @author Andreas Stensig Jensen, on Nov 7, 2014
  * Contributors:
@@ -23,39 +31,31 @@ public class UDPTrafficManager {
     private int rmiPort;
     private int udpSocketPort = 2409;
 
-    // Test for input buffer size with 800 passengers was 8207
+    /**
+     * Size for a <code>byte</code> buffer tested to hold 800 passengers in a
+     * serialized <code>PassengerList</code>. Test showed a size of 8207 bytes.
+     * Actual size is 8,300 for good measure.
+     */
     protected static final int BUFFER_IN_SIZE = 8300;
-    // Test for input buffer size with 800 passengers was 21833
+
+    /**
+     * Size for a <code>byte</code> buffer tested to hold 800 tickets in a
+     * serialized <code>TicketList</code>. Test showed a size of 21,833 bytes.
+     * Actual size is 21,900 for good measure.
+     */
     protected static final int BUFFER_OUT_SIZE = 21900;
 
-// Legacy method
-    public synchronized boolean sendDatagram(DatagramPacket packet) {
-        try {
-            socket.send(packet);
-            return true;
-        } catch (IOException ex) {
-            return false;
-        }
-    }
 
-// Legacy method -- possibly not needed
-// Handlers should themselves determin when to stop execution, via their acks 
-// and seq#. 
-    private boolean closeHandlerThread(String threadName) {
-//        for (UDPPacketHandler handler : handlers) {
-//            if (handler.name().equals(threadName)) {
-//                handler.killThread();
-//                handlers.remove(handler);
-//                return true;
-//            }
-//        }
-        return false;
-    }
-
-    private void openUDPSocket(String host, String port) {
+    /**
+     * Open the UDP <code>DatagramSocket</code> on a specified port, with the
+     * host name of the local machine.
+     * <p>
+     * @param port port number for the socket.
+     */
+    private void openUDPSocket(String port) {
         try {
             int socketPort = Integer.parseInt(port);
-            InetAddress hostAddr = InetAddress.getByName(host);
+            InetAddress hostAddr = InetAddress.getLocalHost();
             socket = new DatagramSocket(socketPort, hostAddr);
         } catch (NumberFormatException | UnknownHostException | SocketException ex) {
             System.err.println("Fatal error in UPDTrafficManager.");
@@ -64,8 +64,17 @@ public class UDPTrafficManager {
         }
     }
 
+    /**
+     * Assigns the RMI properties to field variables. This method MUST be called
+     * prior to the <code>distributeDatagram</code> method.
+     * <p>
+     * @param RMIHost           host name of the RMI registry location.
+     * @param RMIPort           port number of the RMI registry location.
+     * @param RMIJouenryManName name of the <code>JourneyManager</code>
+     *                          implementation class in the RMI registry.
+     */
     private void setRMIpropperties(String RMIHost, String RMIPort,
-                                          String RMIJouenryManName) {
+                                   String RMIJouenryManName) {
         try {
             rmiHost = RMIHost;
             rmiPort = Integer.parseInt(RMIPort);
@@ -76,58 +85,40 @@ public class UDPTrafficManager {
             System.exit(-1);
         }
     }
-    
-// Legacy method -- possibly not needed
+
     /**
-     * Reply to the a client with a <code>DatagramPacket</code> whose payload
-     * is an integer specifying the port number of a new 
-     * <code>DatagramSocket</code> created in the handler thread that will 
-     * service the client. 
-     * @param packet the original <code>DatagramPakcet</code> from the client 
-     * requesting to be serviced. This is used to generate a reply. 
-     * @throws IOException if an I/O exception occurs during the output streams 
-     * generating the reply. 
-     */
-    private void replyWithHandlerPort(DatagramPacket packet) throws IOException {
-        // Create output streams and generate reply
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        ObjectOutputStream oos = new ObjectOutputStream(bos);
-        Integer handlerSocketReply = udpSocketPort;
-        oos.writeObject(handlerSocketReply);
-        byte[] replyArr = bos.toByteArray();
-        
-        // Get destination ip and port and send reply. 
-        InetAddress destAddr = packet.getAddress();
-        int port = packet.getPort();
-        DatagramPacket reply = new DatagramPacket(replyArr, replyArr.length, destAddr, port);
-        socket.send(reply);
-    }
-    
-    /**
-     * Distributes a <code>DatagramPacket</code> to a new handler thread. 
+     * Distributes a <code>DatagramPacket</code> to a new handler thread.
      * The new handler will then take over communication with the client through
-     * its own <code>DatagramSocker</code>. 
-     * @param packet the <code>DatagramPacket</code> received from a client that 
-     * wishes to be serviced. 
+     * its own <code>DatagramSocker</code>.
+     * <p>
+     * The <code>setRMIPropperties</code> method MUST be called prior to this
+     * method.
+     * <p>
+     * @param packet the <code>DatagramPacket</code> received from a client that
+     *               wishes to be serviced.
      */
     private void distributeDatagram(DatagramPacket packet) {
         /*
-        Do not offer port numbers greater than 2,409 + 2,000. With a restiction
-        of a maximum users of 1,000, we can expect to be able to reuse port 
-        numbers after an extra 1,000 port numbers.
-        */
+         Do not offer port numbers greater than 2,409 + 2,000. With a restiction
+         of a maximum users of 1,000, we can expect to be able to reuse port 
+         numbers after an extra 1,000 port numbers.
+         */
         if (udpSocketPort >= 4409) {
             udpSocketPort = 2409;
         }
-        
+
         /*
-        Instantiate new handler for the DatagramPacket, start handler thread, 
-        and increment UDP socket port counter.
-        */
+         Instantiate new handler for the DatagramPacket, start handler thread, 
+         and increment UDP socket port counter.
+         */
         try {
-            // Using test constructor to ignore RMI.
-            UDPPacketHandler handler = 
-                   new UDPPacketHandler(packet, udpSocketPort);
+            // Using test constructor to ignore RMI. 
+            UDPPacketHandler handler
+                    = new UDPPacketHandler(packet, udpSocketPort);
+            // Outcommented to ignore RMI
+            //UDPPacketHandler handler = 
+            //       new UDPPacketHandler(packet, udpSocketPort, rmiHost,
+            //                               rmiPort, rmiJournayManagerName)
             System.err.println("Handler created.");
             handler.start();
             System.err.println("Handler started.");
@@ -139,26 +130,30 @@ public class UDPTrafficManager {
             System.err.println("Continue listening for new datagram.");
         }
     }
-    
+
     /**
      * Main method for starting <code>UDPTrafficManger</code>,
      * <code>UDPPacketHandler</code>, and <code>JourneyManager</code>.
      * <p>
      * @param args
+     *             <ul>
+     * <li>0 : port number for the <code>UDPTrafficManager</code>'s UDP socket.
+     * <li>1 : host name for the RMI registry server.
+     * <li>2 : port number for the RMI registry server.
+     * <li>3 : name of the <code>JourneyManager</code> implementation class in
+     * the RMI registry.
+     * </ul>
      * <p>
-     * @throws UnknownHostException
      */
-    public static void main(String[] args) throws UnknownHostException {
+    public static void main(String[] args) {
         UDPTrafficManager manager = new UDPTrafficManager();
-    /* testing */
-        InetAddress addr = InetAddress.getLocalHost();
+        /* testing */
         String port = "2408";
-        String host = addr.getHostName();
-        manager.openUDPSocket(host, port);
-    /* -- */      
-    // Outcommented for testing
-        //openUDPSocket(args[0], args[1]);
-        //setRMIpropperties(args[2], args[3], args[4]);
+        manager.openUDPSocket(port);
+        /* -- */
+        // Outcommented for testing
+        //manager.openUDPSocket(args[0]);
+        //manager.setRMIpropperties(args[1], args[2], args[3]);
 
         DatagramPacket packet;
         while (true) {
