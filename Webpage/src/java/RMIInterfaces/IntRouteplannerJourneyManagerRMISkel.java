2f0fd5f41b5b7412c81ee24462fafff2ce20a164
qess2000@hotmail.com <qess2000@hotmail.com@2b5b00e0-a592-e1ba-476f-276ff1d89472>
2014-12-01 08:47:13 +0000

    routeplanner update

--- Webpage/src/java/RMIInterfaces/IntRouteplannerJourneyManagerRMISkel.java
+++ Webpage/src/java/RMIInterfaces/IntRouteplannerJourneyManagerRMISkel.java
@@ -15,7 +15,7 @@ public interface IntRouteplannerJourneyManagerRMISkel extends Remote
     public  Time[] getNextDeparture(String type, String line, String fromStop, String towardsStop, int hour, int minute) throws RemoteException;
    // Return is availible departure; timeDeparture is the desired departure time
     
-    public  Stop[] SetupGraph(); 
+    public  Stop[] setupGraph(); 
     
     
 }
