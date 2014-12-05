c768fda8b93d9aaa3b4e0d7eb3023e02aac91e43
Mathias_nr@hotmail.com <Mathias_nr@hotmail.com@2b5b00e0-a592-e1ba-476f-276ff1d89472>
2014-12-05 11:46:32 +0000

    Cleaned up and wrote Javadocs. Fixed String in getJourneyHistory to int

--- SEM3/RMIServer/src/RMIImpl/WebsiteManagerRMISkelImpl.java
+++ SEM3/RMIServer/src/RMIImpl/WebsiteManagerRMISkelImpl.java
@@ -15,36 +15,78 @@ public class WebsiteManagerRMISkelImpl extends UnicastRemoteObject implements We
     }  
     
     /**
-     * int createCustomer(Customer)
-     * @param customer
-     * @return customer number
-     * @throws java.rmi.RemoteException 
-     */   
-    @Override //adds a customer to the SQL database returns the customer number
+    * Invokes the createCustomer method specified in the DatabaseManager.java included
+    * in the Libraries/DatabaseManager.jar.
+    * Implements the method from RMIInterfaces/WebsiteManagerRMISkel.
+    * <P>
+    * Adds a customer to the SQL database.
+    * @param  Customer The customer to be created without customerNumber.
+    * @return      The customerNumber
+    * @see         RMIInterfaces.WebsiteManagerRMISkel
+    * @see         Manager.DatabaseManager
+    * @throws java.rmi.RemoteException 
+    */   
+    @Override
     public int createCustomer(Customer customer) throws RemoteException { 
-    return databasemanager.createCustomer(customer);  
+        return databasemanager.createCustomer(customer);  
     }
-
+    
+    /**
+    * Invokes the setCustomerDetails method specified in the DatabaseManager.java included
+    * in the Libraries/DatabaseManager.jar.
+    * Implements the method from RMIInterfaces/WebsiteManagerRMISkel
+    * <P>
+    * Changes the information on a customer in the SQL database.
+    * @param  Customer The updated customer with customerNumber.
+    * @return      The new data for the customer.
+    * @see         RMIInterfaces.WebsiteManagerRMISkel
+    * @see         Manager.DatabaseManager
+    * @throws java.rmi.RemoteException 
+    */   
     @Override
     public Customer setCustomerDetails(Customer customer) throws RemoteException {
-    return databasemanager.setCustomerDetails(customer);        
+        return databasemanager.setCustomerDetails(customer);        
     }
 
-    @Override
+    /*@Override
     public Customer getCustomerDetails(String customerNumber) throws RemoteException {
-        // insert String to int - try catch protection here
-    return databasemanager.getCustomerDetails(Integer.parseInt(customerNumber));            
-    }             
+        return databasemanager.getCustomerDetails(Integer.parseInt(customerNumber));            
+    }*/        
  
+    /**
+    * Invokes the logOn method specified in the DatabaseManager.java included
+    * in the Libraries/DatabaseManager.jar.
+    * Implements the method from RMIInterfaces/WebsiteManagerRMISkel
+    * <P>
+    * Finds the customer with the specified email/password combination.
+    * @param  email The email address entered by the user
+    * @param password The password entered by the user
+    * @return      A Customer object.
+    * @see         RMIInterfaces.WebsiteManagerRMISkel
+    * @see         Manager.DatabaseManager
+    * @throws java.rmi.RemoteException 
+    */
     @Override
     public Customer logOn(String email, String password) throws RemoteException {
-    return databasemanager.logOn(email, password);  
+        return databasemanager.logOn(email, password);
     }
 
+    /**
+    * Invokes the getJourneyHistory method specified in the DatabaseManager.java included
+    * in the Libraries/DatabaseManager.jar.
+    * Implements the method from RMIInterfaces/WebsiteManagerRMISkel
+    * <P>
+    * Finds the journey objects associated with a customerNumber
+    * @param  customerNumber The users customerNumber
+    * @param index The index to fetch the tickets from
+    * @return ArrayList<Journey> 
+    * @see         RMIInterfaces.WebsiteManagerRMISkel
+    * @see         Manager.DatabaseManager
+    * @throws java.rmi.RemoteException 
+    */
     @Override
-    public ArrayList<Journey> getJourneyHistory(String customerNumber, int index) throws RemoteException {  
-        // insert String to int - try catch protection here
-    return  databasemanager.getJourneyHistory(Integer.parseInt(customerNumber), index);    
+    public ArrayList<Journey> getJourneyHistory(int customerNumber, int index) throws RemoteException {  
+        return  databasemanager.getJourneyHistory(customerNumber, index);    
     }
         
 }      
--- SEM3/RMIServer/src/RMIInterfaces/WebsiteManagerRMISkel.java
+++ SEM3/RMIServer/src/RMIInterfaces/WebsiteManagerRMISkel.java
@@ -1,24 +1,69 @@
 package RMIInterfaces;
 
-//Class from which objects will be created 
-//and then passed as arguments.
-
-
 import ModelClasses.*;
 import java.rmi.*;
 import java.util.ArrayList;
 
 public interface WebsiteManagerRMISkel extends Remote
 {
+    /**
+    * Invokes the createCustomer method specified in the DatabaseManager.java included
+    * in the Libraries/DatabaseManager.jar.
+    * Implements the method from RMIInterfaces/WebsiteManagerRMISkel.
+    * <P>
+    * Adds a customer to the SQL database.
+    * @param  Customer The customer to be created without customerNumber.
+    * @return      The customerNumber
+    * @see         RMIInterfaces.WebsiteManagerRMISkel
+    * @see         Manager.DatabaseManager
+    * @throws java.rmi.RemoteException 
+    */  
     public int createCustomer(Customer customer) throws RemoteException; 
     
+    /**
+    * Invokes the setCustomerDetails method specified in the DatabaseManager.java included
+    * in the Libraries/DatabaseManager.jar.
+    * Implements the method from RMIInterfaces/WebsiteManagerRMISkel
+    * <P>
+    * Changes the information on a customer in the SQL database.
+    * @param  Customer The updated customer with customerNumber.
+    * @return      The new data for the customer.
+    * @see         RMIInterfaces.WebsiteManagerRMISkel
+    * @see         Manager.DatabaseManager
+    * @throws java.rmi.RemoteException 
+    */ 
     public Customer setCustomerDetails(Customer customer) throws RemoteException;
     
     // customer number is stored as an integer in the database... cast integer to string for return
-    public Customer getCustomerDetails(String customerNumber) throws RemoteException; 
+    //public Customer getCustomerDetails(String customerNumber) throws RemoteException; 
 
+    /**
+    * Invokes the logOn method specified in the DatabaseManager.java included
+    * in the Libraries/DatabaseManager.jar.
+    * Implements the method from RMIInterfaces/WebsiteManagerRMISkel
+    * <P>
+    * Finds the customer with the specified email/password combination.
+    * @param  email The email address entered by the user
+    * @param password The password entered by the user
+    * @return      A Customer object.
+    * @see         RMIInterfaces.WebsiteManagerRMISkel
+    * @see         Manager.DatabaseManager
+    * @throws java.rmi.RemoteException 
+    */
     public Customer logOn(String email, String password) throws RemoteException;
    
-    // customer number is stored as an integer in the database... cast integer to string for return
-    public ArrayList<Journey> getJourneyHistory(String customerNumber, int index) throws RemoteException;  
+    /**
+    * Invokes the getJourneyHistory method specified in the DatabaseManager.java included
+    * in the Libraries/DatabaseManager.jar.
+    * Implements the method from RMIInterfaces/WebsiteManagerRMISkel
+    * <P>
+    * Finds the journey objects associated with a customerNumber
+    * @param  customerNumber The users customerNumber
+    * @param index The index to fetch the tickets from
+    * @return ArrayList<Journey> 
+    * @see         RMIInterfaces.WebsiteManagerRMISkel
+    * @see         Manager.DatabaseManager
+    * @throws java.rmi.RemoteException 
+    */
+    public ArrayList<Journey> getJourneyHistory(int customerNumber, int index) throws RemoteException;  
 }
