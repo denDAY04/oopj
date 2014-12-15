56d13e518c70e7437944081ba71756177af12756
Qess2000@hotmail.com <Qess2000@hotmail.com@2b5b00e0-a592-e1ba-476f-276ff1d89472>
2014-12-15 22:52:15 +0000

    small update

--- SEM3/RMIServer/src/RMIInterfaces/RouteplannerJourneyManagerRMISkel.java
+++ SEM3/RMIServer/src/RMIInterfaces/RouteplannerJourneyManagerRMISkel.java
@@ -1,3 +1,9 @@
+/**
+ * RouteplannerJourneyManagerRMISkel 
+ * Interface for RMI connection to Database
+ * @author Rasmus
+ */
+
 package RMIInterfaces;
 
 import java.rmi.*;
@@ -5,13 +11,21 @@ import ModelClasses.*;
 import java.util.ArrayList;
 
 public interface RouteplannerJourneyManagerRMISkel extends Remote
-{      
-    public Stop[] setupGraph() throws RemoteException;
+{
+    /**
+     * Returns an array of Stop objects, representing a graph of all stops and
+     * links between stops, representing busses and trains.
+     * @return
+     * @throws RemoteException
+     */
+    public Stop[] setupGraph() throws RemoteException; 
     
+    /**
+     * Returns an array of Zone objects, representing an adjacency list of 
+     * Zones. This list describes the physical layout of the zones.
+     * @return
+     * @throws RemoteException
+     */
     public ArrayList<Zone> setupZoneDistance() throws RemoteException;
+
 }
-
-
-
-
-
