16e15b8fd1f8b987c12ac32f5a8e3a3f06ce808b
Qess2000@hotmail.com <Qess2000@hotmail.com@2b5b00e0-a592-e1ba-476f-276ff1d89472>
2014-12-15 22:50:37 +0000

    small update

--- SEM3/RMIServer/src/RMIInterfaces/RouteplannerJourneyManagerRMISkel.java
+++ SEM3/RMIServer/src/RMIInterfaces/RouteplannerJourneyManagerRMISkel.java
@@ -1,16 +1,12 @@
 package RMIInterfaces;
 
 import java.rmi.*;
-import java.sql.Time;
 import ModelClasses.*;
 import java.util.ArrayList;
 
 public interface RouteplannerJourneyManagerRMISkel extends Remote
-{
-//    public double getNextDeparture(int startPosition, int endPosition, int timeDeparture) throws RemoteException;
-//   // Return is availible departure; timeDeparture is the desired departure time
-       
-    public Stop[] setupGraph() throws RemoteException; //Objects may be defined as a modelclass Stop[]
+{      
+    public Stop[] setupGraph() throws RemoteException;
     
     public ArrayList<Zone> setupZoneDistance() throws RemoteException;
 }
